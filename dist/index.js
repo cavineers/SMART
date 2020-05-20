25ebf91e518aeff5902647fdc84d0bb87475ded2
topmastered <bcary.md@gmail.com>
2020-05-20 17:36:10 -0400

    cleared

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-console.clear();
-var Constants_1 = require("./Constants");
-var window_1 = require("./window");
-var electron_1 = require("electron");
-console.log("Starting " + Constants_1["default"].app_name.short);
-var k_window;
-window_1["default"].main(electron_1.app, electron_1.BrowserWindow);
--- src/Constants.ts
+++
@@ -1,7 +0,0 @@
-export default class Constants {
-    public static app_name = class {
-        public static long: string = "Super Machine Learning/Artificial Intelligent Robot Tracker";
-        public static short: string = "SMART";
-    };
-    public static version: string = "0.0.1";
-}
